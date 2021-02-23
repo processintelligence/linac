b09ee8086cb06e60ba7b889da0899c5b165f21c1
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2021-02-24 00:20:54 +0100

    Added relative DateTime to the simulator

--- backend/src/controllers/Main.java
+++ backend/src/controllers/Main.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import entities.*;
 import entities.library.*;
 import utils.*;
@@ -7,6 +10,8 @@ import logic.*;
 
 public class Main {
 	public static void main(String[] args) throws InterruptedException {
+		
+		/*
 		Agent testAgent = new Agent("Agent",0,0);
 		System.out.println(testAgent.getPosition().toString());
 		Lamp testSensor = new Lamp("Lamp Sensor", false ,1,1);
@@ -14,15 +19,25 @@ public class Main {
 		
 		System.out.println(testAgent.getPosition().distance(testSensor.getPosition()));
 
-		Log.setFileName("test");
-		Log.createFile();
+		
 		Log.writeToFile("2020-02-11 13:14:00.099", "Thermometer", "21.3");
 		Log.writeToFile("2020-02-11 13:14:01.098", "Kitchen", "True");
 		testSensor.onInteraction();
 		testSensor.onInteraction();
+		*/
 		
+		Log.setFileName("test");
+		Log.createFile();
 		Simulator world1 = new Simulator();
 		world1.startSimulator();
+		
+		
+		/*
+		LocalDateTime datetime = LocalDateTime.of(2020, 1, 1, 0, 0, 0, 0); 
+		System.out.println(datetime);
+		datetime = datetime.plusNanos(16000000);
+		System.out.println(datetime);
+		*/
 		
 	}
 
--- backend/src/logic/Simulator.java
+++ backend/src/logic/Simulator.java
@@ -1,38 +1,54 @@
 package logic;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
+
+import entities.library.Lamp;
 
 public class Simulator {
 	
-	private static long NS_PER_TICK = 16000000;
-	private static int n = 0;
+	private long nsPerTick = 16000000; //UPS == 1000000000 / NS_PER_TICK
+	private int tick = 0;
+	private LocalDateTime datetime = LocalDateTime.of(2020, 1, 1, 0, 0, 0, 0); 
 	
 	public void startSimulator() throws InterruptedException {
-		while (n<60) {
+		//Pre-simulation operations
+		Lamp testSensor = new Lamp("Lamp Sensor", false ,1 ,1);
+		
+		//Simulation Logic-Loop
+		while (tick<60) {
 			long start = System.nanoTime();
+			datetime = datetime.plusNanos(nsPerTick); //updates datetime
+			
 			processInput();
 			update();
 			render();
-			TimeUnit.NANOSECONDS.sleep(start + NS_PER_TICK - System.nanoTime());
-			System.out.print("E");
-			n++;
+			
+			TimeUnit.NANOSECONDS.sleep(start + nsPerTick - System.nanoTime()); //thread sleeps for real-time rendering
+			//System.out.println(start + nsPerTick - System.nanoTime()); //computation time left per loop 
+			//System.out.println(datetime);
+			//testSensor.onInteraction();
+			//System.out.print("E");
+			tick++;
 		}
 	}
 	
 	public void startRenderingSimulator() throws InterruptedException {
-		while (n<60) {
+		while (tick<60) {
 			long start = System.nanoTime();
+			datetime = datetime.plusNanos(nsPerTick);
 			processInput();
 			update();
 			render();
-			TimeUnit.NANOSECONDS.sleep(start + NS_PER_TICK - System.nanoTime());
+			TimeUnit.NANOSECONDS.sleep(start + nsPerTick - System.nanoTime());
 			System.out.print("E");
-			n++;
+			tick++;
 		}
 	}
 
 	private void processInput() {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
 
