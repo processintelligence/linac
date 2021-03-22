7980d1528f1a81354568c4987c380e4c6bba4c0b
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2021-03-22 12:48:48 +0100

    Converted the project  into a Maven project

---
+++ backend/src/main/java/controllers/SystemController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/system/")
+@CrossOrigin
+public class SystemController {
+
+	@GetMapping("/ping")
+	public @ResponseBody String ping() {
+		return "pong";
+	}
+}
