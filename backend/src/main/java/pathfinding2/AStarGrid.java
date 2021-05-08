70dcdd9b97f2f0d42efd11f90a72b0a000bedd63
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2021-05-08 02:06:10 +0200

    make nodes of tiles with non-walkable entities non-walkable

--- backend/src/main/java/pathfinding2/AStarGrid.java
+++ backend/src/main/java/pathfinding2/AStarGrid.java
@@ -66,7 +66,17 @@ public class AStarGrid {
 		// make nodes of tiles with non-walkable sensors not-walkable
 		for (Sensor sensor : floorplan.getSensors()) {
 			if (sensor.getWalkable() == false) {
+				
+				
 				for (Position position : sensor.getPositions()) {
+					setNodeState(position.getX(), position.getY(), NodeState.NOT_WALKABLE);
+				}
+			}
+		}
+		// make nodes of tiles with non-walkable entities not-walkable
+		for (Entity entity : floorplan.getEntities()) {
+			if (entity.getWalkable() == false) {
+				for (Position position : entity.getPositions()) {
 					setNodeState(position.getX(), position.getY(), NodeState.NOT_WALKABLE);
 				}
 			}
