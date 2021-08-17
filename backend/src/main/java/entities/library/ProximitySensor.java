720fd6b4356a87aeb5275e743b6aaf0a117a3801
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2021-08-17 18:27:31 +0200

    ProximitySensor sensor type reads the distance to the nearest agent in its interact area

--- backend/src/main/java/entities/library/ProximitySensor.java
+++ backend/src/main/java/entities/library/ProximitySensor.java
@@ -3,6 +3,7 @@ package entities.library;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 
+import entities.Agent;
 import entities.SensorPassive;
 import geo.Position;
 import main.Resources;
@@ -15,30 +16,46 @@ public class ProximitySensor extends SensorPassive {
 	
 	public void defineDefaultState() {
 		state.put("triggered", false);
-		state.put("proximity", "N/A");
+		state.put("proximity", null);
 	}
 	
 	// Trigger behavior 
 	public boolean updateState() {
-		if (getInteractArea().contains(Resources.getSimulator().getAgent().getPosition())) {
-			state.put("triggered", true);
-			
-			double x = 0;
-			double y = 0;
-			for (Position position : getPhysicalArea()) {
-				x = x + position.getX();
-				y = y + position.getY();
+		
+		if (getPhysicalArea().size() == 0) {
+			state.put("triggered", false);
+			state.put("proximity", null);
+			return false;
+		}
+		
+		Double closestProximity = null;
+		
+		for (Agent agent : Resources.getFloorplan().getAgents()) {
+			if (getInteractArea().contains(agent.getPosition())) {
+				
+				double x = 0;
+				double y = 0;
+				for (Position position : getPhysicalArea()) {
+					x = x + position.getX();
+					y = y + position.getY();
+				}
+				x = x / getPhysicalArea().size();
+				y = y / getPhysicalArea().size();
+				Double proximity = Math.hypot(agent.getPosition().getX() - x, agent.getPosition().getY() - y);
+				
+				if (closestProximity == null || closestProximity > proximity) {
+					closestProximity = proximity;
+				}
 			}
-			x = x / getPhysicalArea().size();
-			y = y / getPhysicalArea().size();
-			Double proximity = Math.hypot(Resources.getSimulator().getAgent().getPosition().getX() - x, Resources.getSimulator().getAgent().getPosition().getY() - y);
-			
-			
-			state.put("proximity", proximity);
+		}
+		
+		if (closestProximity != null) {
+			state.put("triggered", true);
+			state.put("proximity", closestProximity);
 			return true;
 		} else {
 			state.put("triggered", false);
-			state.put("proximity", "N/A");
+			state.put("proximity", null);
 			return false;
 		}
 	}
