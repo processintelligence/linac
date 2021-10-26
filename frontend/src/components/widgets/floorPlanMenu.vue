fca8270599db3b29c6d4d2cc4eddb37a5790233a
dfgemma <gdfe@dtu.dk>
2021-10-26 10:24:53 +0200

    icons adjustments

--- frontend/src/components/widgets/floorPlanMenu.vue
+++ frontend/src/components/widgets/floorPlanMenu.vue
@@ -3,53 +3,50 @@
     <v-card class="m-3">
       <v-list dense>
         <v-list-item>
-          <v-text-field
-            label="Filter"
-            hint="Case-sensitive"
-            v-model="filterText"
-          ></v-text-field>
-        </v-list-item>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          @click="executeEvent(item.eventName)"
-        >
           <v-list-item-content>
-            <v-list-item-title>
+            <v-text-field label="Filter" hint="Case-sensitive" v-model="filterText"></v-text-field>
+          </v-list-item-content>
+        </v-list-item>
+          <v-list-item v-for="item in items" :key="item.title" @click="executeEvent(item.eventName)">
+            <v-list-item-icon>
               <font-awesome-icon :icon=item.icon />
-              {{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+            </v-list-item-icon>
 
-        <v-list-item
-          v-for="item in itemsInline"
-          :key="item.title"
-          class="float-left"
-          @click="executeEvent(item.eventName)"
-        >
-          <v-list-item-content>
-            <v-list-item-title><font-awesome-icon :icon=item.icon /></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-        <v-list-item @click="{}" v-b-modal.modal-upload style="clear: both">
-          <v-list-item-content>
-            <v-list-item-title>
-              <font-awesome-icon icon="upload" /> Upload / Download
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <v-divider />
+          <v-list-item v-for="item in itemsInline" :key="item.title" class="float-left"
+            @click="executeEvent(item.eventName)">
+            <v-list-item-icon>
+              <font-awesome-icon :icon=item.icon />
+            </v-list-item-icon>
 
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-subtitle class="text-wrap">{{
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item @click="{}" v-b-modal.modal-upload style="clear: both">
+            <v-list-item-icon>
+              <font-awesome-icon icon="upload" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                Upload / Download </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+          <v-divider />
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-subtitle class="text-wrap">{{
             tooltip
           }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card>
+            </v-list-item-content>
+          </v-list-item>
+  </v-card>
 
     <b-modal id="modal-upload" title="Upload or download the floor plan">
       <UploadDownload @closeCard="closeCardRefresh" />
@@ -59,53 +56,80 @@
 </template>
 
 <script>
-import UploadDownload from "@/components/widgets/uploadDownload.vue";
-// import About from '../views/About.vue';
-export default {
-  name: "FloorPlanMenu",
-  components: { UploadDownload },
-  data() {
-    return {
-      items: [
-        { title: "Add Sensor", eventName: "gridAddSensor", icon: "lightbulb" },
-        { title: "Add Entity", eventName: "gridAddEntity", icon: "layer-group" },
-        { title: "Add Agent", eventName: "gridAddAgent", icon: "walking" },
-      ],
-      itemsInline: [
-        { eventName: "gridPanUp", icon: "angle-up" },
-        { eventName: "gridPanDown", icon: "angle-down" },
-        { eventName: "gridPanLeft", icon: "angle-left" },
-        { eventName: "gridPanRight", icon: "angle-right" },
-        { eventName: "gridClear", icon: "trash" },
-      ],
-      y: window.innerHeight - 57,
-      x: window.innerWidth * 0.15,
-      filterText: this.$store.state.filterText,
-      showUploadDownload: false,
-      tooltip: "x-y",
-    };
-  },
-  methods: {
-    executeEvent(eventName) {
-      this.$root.$emit(eventName);
+  import UploadDownload from "@/components/widgets/uploadDownload.vue";
+  // import About from '../views/About.vue';
+  export default {
+    name: "FloorPlanMenu",
+    components: {
+      UploadDownload
     },
-    closeCardRefresh() {
-      this.showUploadDownload = false;
+    data() {
+      return {
+        items: [{
+            title: "Add Sensor",
+            eventName: "gridAddSensor",
+            icon: "lightbulb"
+          },
+          {
+            title: "Add Entity",
+            eventName: "gridAddEntity",
+            icon: "layer-group"
+          },
+          {
+            title: "Add Agent",
+            eventName: "gridAddAgent",
+            icon: "walking"
+          },
+        ],
+        itemsInline: [{
+            eventName: "gridPanUp",
+            icon: "angle-up"
+          },
+          {
+            eventName: "gridPanDown",
+            icon: "angle-down"
+          },
+          {
+            eventName: "gridPanLeft",
+            icon: "angle-left"
+          },
+          {
+            eventName: "gridPanRight",
+            icon: "angle-right"
+          },
+          {
+            eventName: "gridClear",
+            icon: "trash"
+          },
+        ],
+        y: window.innerHeight - 57,
+        x: window.innerWidth * 0.15,
+        filterText: this.$store.state.filterText,
+        showUploadDownload: false,
+        tooltip: "x-y",
+      };
     },
-    onResize() {
-      this.y = window.innerHeight - 57;
-      this.x = window.innerWidth * 0.15;
+    methods: {
+      executeEvent(eventName) {
+        this.$root.$emit(eventName);
+      },
+      closeCardRefresh() {
+        this.showUploadDownload = false;
+      },
+      onResize() {
+        this.y = window.innerHeight - 57;
+        this.x = window.innerWidth * 0.15;
+      },
     },
-  },
-  watch: {
-    filterText: function (newText) {
-      this.$store.commit("updateFilterText", newText);
+    watch: {
+      filterText: function (newText) {
+        this.$store.commit("updateFilterText", newText);
+      },
     },
-  },
-  mounted() {
-    this.$root.$on("tooltip", (data) => {
-      this.tooltip = data;
-    });
-  },
-};
+    mounted() {
+      this.$root.$on("tooltip", (data) => {
+        this.tooltip = data;
+      });
+    },
+  };
 </script>
